fd73d6555354b4db0101aba7ca92db959847d050
duchieu <duchieu.ctk41@gmail.com>
2021-02-10 14:52:15 +0700

    3/7 & 5/7 add firebase & authentication

---
+++ src/firebase/firebase.ultis.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyBGN8JyqbPGYoU-AACboBfHuRsGRVRMPMM",
+  authDomain: "crwn-db-db03e.firebaseapp.com",
+  projectId: "crwn-db-db03e",
+  storageBucket: "crwn-db-db03e.appspot.com",
+  messagingSenderId: "674620307662",
+  appId: "1:674620307662:web:b6b81ce360fd609313f89f",
+  measurementId: "G-XSG671DKMD",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
