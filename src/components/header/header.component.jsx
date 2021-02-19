ebc7324fe0483a9ffa89839ac29dd9e90458c00c
duchieu <duchieu.ctk41@gmail.com>
2021-02-19 15:18:56 +0700

    11/8 cart component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {  connect } from "react-redux";
+import { connect } from "react-redux";
 
 import { auth } from "../../firebase/firebase.ultis";
+import CartIcon from "../cart-icon/cart-icon.component";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
@@ -29,6 +30,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
